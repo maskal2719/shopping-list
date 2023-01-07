c2b83552b43592eb9c5a567c323afb087ec7ae92
Stanislav Moskalenko <maskal2719@gmail.com>
2023-01-07 21:49:43 +0300

    fix: resize img

--- shopping-list/src/Header.jsx
+++ shopping-list/src/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     return (
         <div>
             <header>
-                <img style={{width:'50px'}} src={logo} alt='logo'/>
+                <img style={{width:'60px'}} src={logo} alt='logo'/>
                 <span className='title'>Header</span>
             </header>
         </div>
